10e087e3d8ccae09bbd34c5a59866b345428f763
Javier Barroso <javibarroso@gmail.com>
2019-10-22 00:22:14 +0200

    Simplify script, not use wait

--- horserace.bash
+++ horserace.bash
@@ -1,33 +1,49 @@
 #!/bin/bash
 
-i=0
-for f in output{0..7}.log
-do
-	echo -n Caballo $i ... > $f
-	let i=i+1
-done
-INI=$SECONDS
-while true 
-do
-	i=$((RANDOM%8))
-	fichero="output$i.log"
-	sleep 0.5;
-	sed -i -s 's,🐎, ,' $fichero
-	echo -n 🐎 >> $fichero
-	if test $((SECONDS-INI)) -gt 120
-	then
-		exit 1
-	fi	
-done  &
-
-tput civis
-while [ -n "$(jobs 2>&1)" ]
-do
-	tput sc
+function print_horses()
+{
 	for f in output*log;
 	do
 		sed -z 's,\([^\n]\)$,\1\n,' $f
 	done
-	tput rc
+}
+
+function finish()
+{
+	print_horses
+	winer=$(wc -c output*log | sort -n | grep -v total | 
+		sed -n -e '$s,.*t,,' -e '$s,\..*,,p')
+	echo "Horse $winer win!"
+	\rm -f output*log
+	tput cvvis
+	exit 0
+}
+
+trap finish exit
+
+
+i=0
+for f in output{0..7}.log
+do
+	echo -n Horse $i ... > $f
+	let i=i+1
 done
-tput cvvis
+INI=$SECONDS
+tput civis
+while true 
+do
+	i=$((RANDOM%8))
+	fichero="output$i.log"
+	sleep 0.1;
+	sed -i -s 's,🐎, ,' $fichero
+	echo -n 🐎 >> $fichero
+	if test $((SECONDS-INI)) -gt 20
+	then
+		exit 0
+	else
+		tput sc
+		print_horses
+		tput rc
+	fi	
+done  
+exit 0
